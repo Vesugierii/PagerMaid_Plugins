ed0c7925ff9e6afb0d05a9b686768ddc7d798349
Vesugierii <66554786+Vesugierii@users.noreply.github.com>
2021-07-31 18:53:33 +0800

    增加行列功能？

--- square.py
+++ square.py
@@ -2,18 +2,20 @@ from pagermaid.listener import listener
 from pagermaid.utils import alias_command
 
 @listener(is_plugin=True, incoming=True, outgoing=True, command=alias_command("square"),
-          description="生成文本矩阵")
+          description="生成文本矩阵，先行后列,后文本")
 async def square(context):
-    num = context.parameter[0]
-    num = int(num)
+    hang = context.parameter[0]
+    hang = int(hang)
+    lie = context.parameter[1]
+    lie = int(lie)
     text = ""
     result = ""
-    for n in range(num+1):
-        if n == num:
+    for n in range(hang+1):
+        if n == hang:
             await context.edit(result)
         else:
-            for m in range(num):
-                text += context.parameter[1]
+            for m in range(lie):
+                text += context.parameter[2]
             result += text
             result += "\n"
             text = ""
