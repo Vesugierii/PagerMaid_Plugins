c0cf45c8b22ef3e6f1ce9f13768b1463857e0c77
Vesugierii <66554786+Vesugierii@users.noreply.github.com>
2021-07-31 19:14:10 +0800

    增加空格功能？

--- square.py
+++ square.py
@@ -2,20 +2,26 @@ from pagermaid.listener import listener
 from pagermaid.utils import alias_command
 
 @listener(is_plugin=True, incoming=True, outgoing=True, command=alias_command("square"),
-          description="生成文本矩阵，先行后列,后文本")
+          description="生成文本矩形，任意值填东西就带空格分隔",
+          parameters="<行> <列> <文本> <任意值>")
 async def square(context):
     hang = context.parameter[0]
     hang = int(hang)
     lie = context.parameter[1]
     lie = int(lie)
-    text = ""
+    desp = ""
     result = ""
+    text = context.parameter[2]
+    if len(context.parameter) == 4:
+        text += " "
+    else:
+        pass
     for n in range(hang+1):
         if n == hang:
             await context.edit(result)
         else:
             for m in range(lie):
-                text += context.parameter[2]
-            result += text
+                desp += text
+            result += desp
             result += "\n"
-            text = ""
+            desp = ""
