009f7c211d59f551491949befbe8a0dde4a26f14
Xtao_dada <xtao@xtaolink.cn>
2021-07-31 20:58:06 +0800

    Update square.py

--- square.py
+++ square.py
@@ -1,27 +1,35 @@
 from pagermaid.listener import listener
 from pagermaid.utils import alias_command
 
-@listener(is_plugin=True, incoming=True, outgoing=True, command=alias_command("square"),
-          description="生成文本矩形，任意值填东西就带空格分隔",
-          parameters="<行> <列> <文本> <任意值>")
+@listener(is_plugin=True, outgoing=True, command=alias_command("square"),
+          description="生成文本矩形",
+          parameters="<行> <列> <文本>")
 async def square(context):
+    if not len(context.parameter):
+        await context.edit('出错了呜呜呜 ~ 参数错误。')
+        return
     hang = context.parameter[0]
-    hang = int(hang)
     lie = context.parameter[1]
-    lie = int(lie)
-    desp = ""
-    result = ""
-    text = context.parameter[2]
-    if len(context.parameter) == 4:
-        text += " "
+    try:
+        hang = int(hang)
+        lie = int(lie)
+    except ValueError:
+        await context.edit('出错了呜呜呜 ~ 参数错误。')
+        return
+    if hang < 1 or lie < 1:
+        await context.edit('出错了呜呜呜 ~ 参数错误。')
+        return
+    parameters = context.parameter
+    del parameters[0]
+    del parameters[0]
+    text = " ".join(parameters)
+    text *=  lie
+    text = text.strip()
+    result = []
+    for i in range(hang):
+        result.append(text)
+    result = '\n'.join(result)
+    if len(result) > 4096:
+        await context.edit('出错了呜呜呜 ~ 文本过长。')
     else:
-        pass
-    for n in range(hang+1):
-        if n == hang:
-            await context.edit(result)
-        else:
-            for m in range(lie):
-                desp += text
-            result += desp
-            result += "\n"
-            desp = ""
+        await context.edit(result)
